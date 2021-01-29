d39d711e7c326091c53f97c196d166eb545e16a7
Eyhenij <Eyhenij@gmail.com>
2021-01-29 18:24:56 +0300

    29.01.2021-002 refactor: apply dotenv lib for port-number and host-name

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const usersRouter = require('./routes/api/usersRouter.js');
 const authRouter = require('./routes/api/authRouter.js');
 require('dotenv').config();
 
-const port = 3000;
-const hostname = 'localhost';
 const app = express();
 
 // body parser middleware
@@ -15,6 +13,6 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/users', usersRouter);
 app.use('/api/login', authRouter);
 
-app.listen(port, hostname, () => {
-    console.log(`Server has been running at http://${hostname}:${port}`);
+app.listen(process.env.PORT, process.env.HOST_NAME, () => {
+    console.log(`Server has been running at http://${process.env.HOST_NAME}:${process.env.PORT}`);
 });
